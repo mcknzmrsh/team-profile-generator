07edf6b8870dded9e91ed709868143b45269e1f1
mcknzmrsh <marsh.mackenzie@gmail.com>
2022-11-23 17:34:18 -0500

    added code to files

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
-// link to page creation
+
 const generateHTML = require('./dist/src/generateHTML');
 
-// team profiles
+
 const Manager = require('./dist/lib/Manager');
 const Engineer = require('./dist/lib/Engineer');
 const Intern = require('./dist/lib/Intern'); 
-
-// node modules 
+ 
 const fs = require('fs'); 
 const inquirer = require('inquirer');
 
-// team array
+
 const teamArray = []; 
 
-// start of manager prompts 
 const addManager = () => {
     return inquirer.prompt ([
         {
