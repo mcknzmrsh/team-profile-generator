cd281524d6d15af23351faf797507a43e722f619
mcknzmrsh <marsh.mackenzie@gmail.com>
2022-11-23 14:15:06 -0500

    added code to tests

--- tests/Manager.test.js
+++ tests/Manager.test.js
@@ -1,1 +1,13 @@
-const Manager = require('.dist/lib/Manager');
+const Manager = require('.dist/lib/Manager')
+
+test('creates a Manager Object', () => {
+    const manager = new Manager ('Noah', 0602, 'noah@mail.com')
+        expect(manager.office.Number).toEqual(expect.any(Number));
+});
+
+
+test('gets role of employee', () => {
+    const manager = new Manager('Noah', 0602, 'noah@mail.com');
+    expect(manager.getRole()).toEqual("Manager")
+
+});
